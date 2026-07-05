bd3674e70c401be7a5133a74b799d86e64069005
llinj571648 <86813069+llinj571648@users.noreply.github.com>
2026-07-05 17:51:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 const app = document.getElementById("app");
-
 const data = window.data;
 
 Object.keys(data).forEach(category => {
 
-  const items = data[category]; // 这是数组
+  const items = data[category];
+
+  const pageName = category
+    .toLowerCase()
+    .replace(/\s+/g, "_") + ".html";
 
   const card = document.createElement("div");
   card.className = "card";
 
-  // 取第一个 item 的 url 当入口（或你也可以改成 category 页面）
-  const first = items[0];
-
   card.innerHTML = `
-    <a href="${first?.url || "#"}">
+    <a href="${pageName}">
       <h3>${category}</h3>
-      <p>${first?.desc || ""}</p>
-      <small>${items.length} sites</small>
+      <p>${items.length} sites</p>
     </a>
   `;
 
